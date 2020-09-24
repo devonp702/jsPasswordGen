a36bac3e983fa495614a40fe767b2a2099984ac5
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-09-24 17:29:02 -0600

    First working version, some bugs to squish.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # jsPasswordGen
+[Lives Here](https://github.com/devonp702/jsPasswordGen)
+
+## Introduction
+Website uses javascript to create a unique custom password when button is pushed. 
+script.js written by Devon Phillips
+
+## Password Creation
+I will put some features, and information about the password creation process here.
--- script.js
+++ script.js
@@ -15,23 +15,58 @@ generateBtn.addEventListener("click", writePassword);
 //the password gets designed here
 function generatePassword() {
   alert("You are about to see some messages. Please read them and choose your answers to customize your password")
-  var pwdStr = "placeholder"; //placeholder for the finished password
+  // declaring variables that customize the output
+  var pwdStr = ""; //placeholder for the finished password
   var includeLower = confirm("Would you like to include lower case characters?");
   var includeUpper = confirm("Would you like to include upper case characters?");
   var includeSymbol = confirm("Would you like to include special characters?");
-  var includeNumber = confirm("Would you like to include numaric characters?");
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var includeNumber = confirm("Would you like to include numeric characters?");
+  var pwdLen = prompt("How many characters would you like to include?");
+  //variables used in generating the password
+  var numbers = "0123456789";
   var lowers = "abcdefghijklmnopqrstuvwxyz";
   var uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var specials = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-  function getRand(string, num) {
-    var chunk = ""
-    for (var i = 0; i < num; i++){
-      chunk += string.charAt();
+
+  function getRand(string, num) { // returns some characters from each list
+    var chunk = "";
+    for (var i = 0; i < num; i++) {
+      chunk += string.charAt(Math.floor(Math.random() * string.length));
     }
-    return chunk
+    return chunk;
   }
-  
-  console.log(chunk)
+
+  function sectorizor(number) {
+    var sctrs = 0;
+    if (includeLower) {
+      sctrs++;
+    }
+    if (includeUpper) {
+      sctrs++;
+    }
+    if (includeNumber) {
+      sctrs++;
+    }
+    if (includeSymbol) {
+      sctrs++;
+    }
+    return number/sctrs;
+  }
+
+  var getNum = sectorizor(pwdLen)
+
+  if (includeLower) {
+    pwdStr += getRand(lowers, getNum);
+    if (includeUpper) {
+      pwdStr += getRand(uppers, getNum);
+    }
+    if (includeNumber) {
+      pwdStr += getRand(numbers, getNum);
+    }
+    if (includeSymbol) {
+      pwdStr += getRand(specials, getNum);
+    }
+  }
+
   return (pwdStr); //this delivers the final password
 }
